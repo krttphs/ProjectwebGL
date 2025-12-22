d8759efe03cfe65889779cdf6fc9921cc43db646
Krit <krittaphas.r@ku.th>
2025-12-23 01:17:49 +0700

    combine unity webGL and frontend

--- routes/quests.js
+++ routes/quests.js
@@ -1,4 +1,3 @@
-// routes/quests.js
 const express = require("express");
 const router = express.Router();
 const supabase = require("../config/supabaseClient");
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 
 const questRoutes = require("./routes/quests");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, "public"),{ index: false }));
 // Routes
 app.use("/api", questRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 // Frontend Route
 app.get("/", (req, res) => {
@@ -29,6 +31,15 @@ app.get("/", (req, res) => {
     res.sendFile("welcome.html", {root: path.join(__dirname, "public")});
   }
 });
+
+app.get("/gamemode1",(req,res)=>{
+  const token = req.cookies.token;
+  if(token){
+    res.sendFile("gameMode1.html",{root: path.join(__dirname,"public")});
+  } else {
+    res.sendFile("welcome.html", {root: path.join(__dirname, "public")});
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
