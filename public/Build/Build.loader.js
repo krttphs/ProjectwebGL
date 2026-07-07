db9bdb470e689b220aa2f77e3f23702b91632c25
krttphs <inadog99@outlook.co.th>
2026-07-08 03:49:58 +0700

    game ui , editor freeze

--- public/Build/Build.loader.js
+++ public/Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 103529429,
+    wasmFileSize: 103531620,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
